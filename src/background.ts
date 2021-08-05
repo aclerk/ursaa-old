86f97e45d37e20db8771605b64a3e3c8318bec10
zhaojj11 <757355084@qq.com>
2021-08-06 02:23:56 +0800

    :tada: 初始化项目

---
+++ src/background.ts
@@ -0,0 +1,85 @@
+'use strict';
+
+import { app, protocol, BrowserWindow } from 'electron';
+import { createProtocol, installVueDevtools } from 'vue-cli-plugin-electron-builder/lib';
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+let win: BrowserWindow | null;
+protocol.registerSchemesAsPrivileged([
+  {
+    scheme: 'app',
+    privileges: {
+      secure: true,
+      standard: true
+    }
+  }
+]);
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 1000,
+    height: 690,
+    minWidth: 1000,
+    minHeight: 690,
+    useContentSize: true,
+    webPreferences: {
+      // Use pluginOptions.nodeIntegration, leave this alone
+      // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
+      nodeIntegration: true
+    }
+  });
+
+  if (process.env.WEBPACK_DEV_SERVER_URL) {
+    win.loadURL(process.env.WEBPACK_DEV_SERVER_URL).then(r => {
+      console.log(r);
+    });
+    if (!process.env.IS_TEST) win.webContents.openDevTools();
+  } else {
+    createProtocol('app');
+    win.loadURL('http://localhost:8080').then(r => {
+      console.log(r);
+    });
+  }
+
+  win.on('closed', () => {
+    win = null;
+  });
+}
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow();
+  }
+});
+
+app.on('ready', async () => {
+  if (isDevelopment && !process.env.IS_TEST) {
+    try {
+      await installVueDevtools();
+    } catch (e) {
+      console.error('Vue Devtools failed to install:', e.toString());
+    }
+  }
+  createWindow();
+});
+
+if (isDevelopment) {
+  if (process.platform === 'win32') {
+    process.on('message', data => {
+      if (data === 'graceful-exit') {
+        app.quit();
+      }
+    });
+  } else {
+    process.on('SIGTERM', () => {
+      app.quit();
+    });
+  }
+}
