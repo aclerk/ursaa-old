ea033126875a83100379f8b13da838fd9f28dda3
zhaojj11 <757355084@qq.com>
2021-09-15 15:17:46 +0800

    :construction: :art: :lipstick: note 代码优化,样式调整

--- src/editor/editor.ts
+++ src/editor/editor.ts
@@ -1,4 +1,5 @@
 export default class Editor {
+  /** 按键枚举 */
   static key = {
     TAB: 9,
     ENTER: 13,
@@ -12,19 +13,22 @@ export default class Editor {
     SHIFT: 16,
     ALT: 18
   };
-  public textAreaId = 'daily_editor';
+  static BUTTONS_TOGGLED_CLASSNAME = 'buttons_toggled';
+  static defaultSettings = {};
+  static textAreaId = 'daily_editor';
   public resultTextarea: HTMLElement | null;
-  public toolbarOpened: boolean | undefined;
+  public toolbarOpened = false;
   public allTools = ['header', 'picture', 'list', 'quote', 'code', 'twitter', 'instagram', 'smile'];
   public settings!: Record<string, any>;
 
-  // HTMLElement
+  /** 编辑器包装类 */
   public wrapper = Editor.createEditorWrapper();
+  public toolbarButtons!: HTMLElement;
 
   constructor(settings: Record<string, any>) {
-    this.resultTextarea = document.getElementById(this.textAreaId);
+    this.resultTextarea = document.getElementById(Editor.textAreaId);
     if (typeof this.resultTextarea == undefined || this.resultTextarea == null) {
-      console.warn('Textarea not found with ID %o', this.textAreaId);
+      console.warn('Textarea not found with ID %o', Editor.textAreaId);
       return this;
     }
 
@@ -36,18 +40,14 @@ export default class Editor {
     } else {
       // todo merge setttings with defaults
     }
-    if ('undefined' == typeof settings.tools || !Array.isArray(settings.tools)) {
-      settings.tools = this.allTools;
-    }
+
+    settings.tools = settings.tools || this.allTools;
     this.settings = settings;
 
-    /** Some configurations */
-    this.toolbarOpened = false;
-
-    /** Making a wrapper and interface */
+    /** 包装接口 */
     this.makeInterface();
 
-    /** Bind all events */
+    /** 绑定事件 */
     this.bindEvents();
   }
 
@@ -55,19 +55,23 @@ export default class Editor {
     const firstNode = Editor.createTextNode(
       'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro quia nihil repellendus aut cupiditate reprehenderit sapiente magnam nobis doloremque eaque! Sint nobis assumenda nisi ducimus minima illo tenetur, cumque facilis.'
     );
-
-    /** Add first node */
+    this.toolbarButtons = this.createToolbarButtons();
+    const toolbar = Editor.createToolbar();
+    console.log(toolbar);
+    /** 添加第一个文本节点 */
     this.wrapper.appendChild(firstNode);
 
-    /** Insert Editor after initial textarea. Hide textarea */
+    /** 创建toolbar */
+    this.wrapper.appendChild(toolbar);
+
+    /** 在原始的textarea后面插入编辑组件,并且隐藏textarea */
     if (this.resultTextarea && this.resultTextarea.parentNode) {
       this.resultTextarea.parentNode.insertBefore(this.wrapper, this.resultTextarea.nextSibling);
       this.resultTextarea.hidden = true;
     }
 
-    /** Set auto focus */
-    const contentEditable = firstNode.getElementsByClassName('ce_node_content');
-    contentEditable.length && (contentEditable[0] as HTMLElement).focus();
+    /** 自动focus */
+    Editor.focusNode(firstNode);
   }
 
   public bindEvents(): void {
@@ -86,33 +90,58 @@ export default class Editor {
 
   private globalKeydownCallback(event: any) {
     switch (event.keyCode) {
+      case Editor.key.TAB:
+        this.tabKeyPressed(event);
+        break; // TAB
       case Editor.key.ENTER:
         this.enterKeyPressed(event);
         break; // Enter
     }
   }
 
+  private tabKeyPressed(event: any) {
+    // check if currently focused in contenteditable element
+    if ('BODY' === event.target.tagName) return;
+    const toolbar = event.target.parentNode.nextSibling;
+    toolbar.appendChild(this.toolbarButtons);
+
+    // eslint-disable-next-line @typescript-eslint/no-this-alias
+    const that = this;
+    setTimeout(() => {
+      if (!toolbar.className.includes(Editor.BUTTONS_TOGGLED_CLASSNAME)) {
+        toolbar.className = toolbar.className.trim() + ' ' + Editor.BUTTONS_TOGGLED_CLASSNAME;
+        that.toolbarOpened = true;
+      } else {
+        toolbar.className = toolbar.className.replace(Editor.BUTTONS_TOGGLED_CLASSNAME, '');
+        that.toolbarOpened = false;
+      }
+    });
+
+    event.preventDefault();
+  }
+
   private enterKeyPressed(event: any) {
     const newNode = Editor.createTextNode();
+    const toolbar = Editor.createToolbar();
 
     this.wrapper.insertBefore(newNode, event.target.parentNode.nextSibling);
 
-    /** Set auto focus */
-    const contentEditable = newNode.getElementsByClassName('ce_node_content');
-    contentEditable.length && (contentEditable[0] as HTMLElement).focus();
+    this.wrapper.insertBefore(toolbar, newNode);
+
+    /** 自动focus */
+    Editor.focusNode(newNode);
 
     event.preventDefault();
   }
 
   /**
-   * Paragraph node
-   * @todo set unique id with prefix
+   * 创建文本节点
    */
   private static createTextNode(content = ''): HTMLElement {
     const node = document.createElement('div');
 
     node.className += 'node';
-    node.innerHTML = '<p class="ce_node_content" contenteditable="true">' + (content || '') + '</p>';
+    node.innerHTML = '<p class="daily_editor_node_content" contenteditable="true">' + (content || '') + '</p>';
 
     return node;
   }
@@ -121,5 +150,58 @@ export default class Editor {
     const wrapper = document.createElement('div');
     wrapper.className += 'daily_editor';
     return wrapper;
+  }
+
+  private static focusNode(node: HTMLElement): void {
+    const contentEditable = node.getElementsByClassName('daily_editor_node_content');
+    contentEditable.length && (contentEditable[0] as HTMLElement).focus();
+  }
+
+  /**
+   * 创建空的toolbar
+   * @private
+   */
+  private static createToolbar() {
+    const bar = document.createElement('div');
+
+    bar.className += 'add_buttons';
+
+    bar.innerHTML = '<span class="toggler">' + '<i class="plus_btn daily_editor_icon-plus-circled-1"></i>' + '</span>';
+    return bar;
+  }
+
+  /**
+   * 根据type创建toolbar 按钮
+   * @param type 类型
+   * @private
+   */
+  private static createToolbarButton(type: string) {
+    const button = document.createElement('button');
+
+    button.dataset.type = type;
+    button.innerHTML = '<i class="daily_editor_icon-' + type + '"></i>';
+
+    return button;
+  }
+
+  /**
+   * 创建所有toolbar 按钮
+   * @private
+   */
+  private createToolbarButtons() {
+    const toolbarButtons = document.createElement('span');
+    toolbarButtons.classList.add('buttons');
+
+    for (let i = 0; i < this.allTools.length; i++) {
+      const tool = this.allTools[i];
+
+      if (this.settings.tools.indexOf(tool) < 0) {
+        continue;
+      }
+
+      const button = Editor.createToolbarButton(tool);
+      toolbarButtons.appendChild(button);
+    }
+    return toolbarButtons;
   }
 }
